025a6574560bfff49bb131ceb39c5797243920b3
Irina Finogenova <finogenova123@rambler.ru>
2022-05-13 20:17:10 +0300

    add galery2

---
+++ homework14/galery/galery2.js
@@ -0,0 +1,14 @@
+function first() {
+    let catImage=document.getElementById('catImage');
+    catImage.src="https://www.fresher.ru/manager_content/images/krasivie-i-bolshie-koshki/big/19.jpg";
+}
+
+function second() {
+    let catImage=document.getElementById('catImage');
+    catImage.src="https://www.fresher.ru/manager_content/images/krasivie-i-bolshie-koshki/15.jpg";
+}
+
+function third() {
+    let catImage=document.getElementById('catImage');
+    catImage.src="https://www.fresher.ru/manager_content/images/krasivie-i-bolshie-koshki/14.jpg";
+}
