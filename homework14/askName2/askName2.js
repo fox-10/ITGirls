d54c05f1d6c9d30a29192dfb36593d99d6fbfdf6
Irina Finogenova <finogenova123@rambler.ru>
2022-05-13 19:17:26 +0300

    add 3 homeworks

---
+++ homework14/askName2/askName2.js
@@ -0,0 +1,4 @@
+function h(){
+    let a= document.getElementById("askName").value;
+    let c=`Привет, ${a}!`;
+    document.getElementById("hi").innerHTML=c;}
---
+++ homework14/galery/galery.js
@@ -0,0 +1,9 @@
+function back() {
+    let catImage=document.getElementById('catImage');
+    catImage.src="https://www.fresher.ru/manager_content/images/krasivie-i-bolshie-koshki/big/19.jpg";
+}
+
+function forward() {
+    let catImage=document.getElementById('catImage');
+    catImage.src="https://www.fresher.ru/manager_content/images/krasivie-i-bolshie-koshki/15.jpg";
+}
