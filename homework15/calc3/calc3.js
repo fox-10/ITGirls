77ceaeb740febf08cf7c3e3f7c55bd44d1cbc4e4
Irina Finogenova <finogenova123@rambler.ru>
2022-05-15 11:35:05 +0300

    add calc4

---
+++ homework15/calc3/calc3.js
@@ -0,0 +1,25 @@
+function s(){
+    let a= parseInt(document.getElementById("sum1").value);
+    let b= parseInt(document.getElementById("sum2").value);
+    let c=a+b;
+    document.getElementById("sum").innerHTML=c;}
+
+function c(){
+    let a= parseInt(document.getElementById("sub1").value);
+    let b= parseInt(document.getElementById("sub2").value);
+    let c=a-b;
+    document.getElementById("sub").innerHTML=c;}
+
+function d(){
+    let a= parseInt(document.getElementById("div1").value);
+    let b= parseInt(document.getElementById("div2").value);
+    let c=a/b;
+    document.getElementById("div").innerHTML=c;}
+    
+function m(){
+    let a= parseInt(document.getElementById("mul1").value);
+    let b= parseInt(document.getElementById("mul2").value);
+    let c=a*b;
+    document.getElementById("mul").innerHTML=c;}
+
+   // if(document.getElementById("div").value="0")  document.getElementById("div").innerHTML='на ноль делить нельзя';
