32295c5796a6fa8daacfaa4e4eb090b6a86efd6e
Irina Finogenova <finogenova123@rambler.ru>
2022-08-30 15:11:27 +0300

    change2 nasa

--- homework20/nasa.js
+++ homework20/nasa.js
@@ -8,14 +8,12 @@ async function loadData() {
     let imgNode = document.createElement('img');
     let textNode = document.createElement('p');
 
-    if(data == "")  {
-        textNode.textContent = "Сервер недоступен";
-    } else {
-        titleNode.textContent = data.title;
+
+    titleNode.textContent = data.title;
     imgNode.src = data.url;
     imgNode.width = 1200;    
     textNode.textContent = data.explanation;
-    }
+    
 
 
     document.body.append(resultNode);
@@ -25,11 +23,21 @@ async function loadData() {
 }
 
 
-loadData();
+// loadData();
 loadData().catch((e) => {if(e){
+    
     let resultNode = document.createElement('div');
+    let titleNode = document.createElement('h1');
+    let imgNode = document.createElement('img');
     let textNode = document.createElement('p');
 
+    titleNode.textContent = "";
+    imgNode.src = "";
+    textNode.textContent = `Ошибка! Тип ошибки: ${e}`;
+
     document.body.append(resultNode);
+    resultNode.append(titleNode);
+    resultNode.append(imgNode);
     resultNode.append(textNode);
-    textNode.textContent = `Ошибка! Тип ошибки: ${e}`;}});
+}});
+
